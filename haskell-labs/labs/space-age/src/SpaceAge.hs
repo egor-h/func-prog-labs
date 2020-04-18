0c7d7a6689a0762ae20bb0383a535ac6800cea1c
Yegor H <procezorjk5@gmail.com>
2020-04-18 13:19:13 +0500

    another one

--- haskell-labs/labs/space-age/src/SpaceAge.hs
+++ haskell-labs/labs/space-age/src/SpaceAge.hs
@@ -1,5 +1,11 @@
 module SpaceAge (Planet(..), ageOn) where
 
+toYears :: Float -> Float -> Float
+toYears seconds coeff = seconds /( 60 * 60 * 24 * 365.25 * coeff)
+
+-- toDays :: Float -> Float -> Float
+-- toDays seconds coeff = seconds /( 60 * 60 * 24 * coeff)
+
 data Planet = Mercury
             | Venus
             | Earth
@@ -10,4 +16,11 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds =
+ageOn Mercury seconds = toYears seconds 0.2408467
+ageOn Venus   seconds = toYears seconds 0.61519726
+ageOn Earth   seconds = toYears seconds 1.0
+ageOn Mars    seconds = toYears seconds 1.8808158
+ageOn Jupiter seconds = toYears seconds 11.862615
+ageOn Saturn  seconds = toYears seconds 29.447498
+ageOn Uranus  seconds = toYears seconds 84.016846
+ageOn Neptune seconds = toYears seconds 164.79132
