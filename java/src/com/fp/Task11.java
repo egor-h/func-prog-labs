0519d2f8b0702f4ebb9eef8ace63434b1d3d81d8
Yegor H <procezorjk5@gmail.com>
2020-04-18 12:46:28 +0500

    asd

--- java/src/com/fp/Task11.java
+++ java/src/com/fp/Task11.java
@@ -1,7 +1,7 @@
 package com.fp;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors;Ф
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
