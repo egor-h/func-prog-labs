3ed82a057a98dfaad219db11733e683788e71a6a
Yegor H <procezorjk5@gmail.com>
2020-04-18 13:45:16 +0500

    asdasd

--- haskell-labs/labs/diamond/src/Diamond.hs
+++ haskell-labs/labs/diamond/src/Diamond.hs
@@ -2,14 +2,19 @@ module Diamond (diamond) where
 
 import Data.Char;
 
+-- getSpaces :: Int -> String
+-- getSpaces 0 = ""
+-- getSpaces 1 = " "
+-- getSpaces n = " " : getSpaces (n - 1)
 
-charWithIdent :: Char -> String
-charWithIdent c = let charNum = ord c
-                  in 
+-- charWithIdent :: Char -> String
+-- charWithIdent c width = let charNum = (ord c) - (ord base)
+--                             base = 'A'
+--                         in 
 
-diamond :: Char -> Maybe [String]
-diamond c = if | c < 'A' -> Nothing
-               | otherwise -> diamond c
-               | (charWithIdent c) : diamond 
-            where
-                lowerChar c = chr ((ord c) - 1)
+-- diamond :: Char -> Maybe [String]
+-- diamond c = if | c < 'A' -> Nothing
+--                | otherwise -> diamond c
+--                | (charWithIdent c) : diamond 
+--             where
+--                 lowerChar c = chr ((ord c) - 1)
